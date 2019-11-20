9b27da3c53ed2cc7ce38b1e9038f3668ad00e2c5
Antoine <assali.antoine@gmail.com>
2019-11-20 12:28:17 +0100

    House-detail stable

--- src/app/houses/house-details/house-details.component.ts
+++ src/app/houses/house-details/house-details.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { House } from '../House';
+import { ActivatedRoute } from '@angular/router';
+import { HousesService } from '../houses.service';
+import { CharactersService } from '../../characters/characters.service';
+import { Character } from 'src/app/characters/Character';
 
 @Component({
   templateUrl: './house-details.component.html',
   styleUrls: ['./house-details.component.scss'],
 })
 export class HouseDetailsComponent implements OnInit {
-  
-  public constructor() {}
+  public characters!: Character[];
+  public house!: House;
+  public constructor(private route: ActivatedRoute, private housesService: HousesService,private charactersService: CharactersService) {}
 
-  public ngOnInit(): void {}
+  public ngOnInit(): void {
+    const { id } = this.route.snapshot.params;
+    this.housesService.getHouse(id).subscribe((house)=> {
+      this.house = house
+    });
+    this.charactersService.listHouseCharacters(id).subscribe((characters)=>{
+      this.characters = characters
+    });
+    
+  }
 }
